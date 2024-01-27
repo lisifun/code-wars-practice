6582a14213a6a2cee85dc478f1b58e96087ea0d9
lisifun <lisandrafj99@gmail.com>
2024-01-27 11:51:14 -0500

    adding 4 codewars

--- bitCounting.js
+++ bitCounting.js
@@ -6,7 +6,6 @@ You can guarantee that input is non-negative.
 Example: 
 
 The binary representation of 1234 is 10011010010, so the function should return 5 in this case
-
 */
 
 var countBits = function (n) {
