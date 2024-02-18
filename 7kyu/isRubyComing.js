d72fd2cfab1ae5ddbcff51e1f790be1f8bbb7d21
lisifun <lisandrafj99@gmail.com>
2024-02-18 15:17:04 -0500

    adding isRubyComing codewars

---
+++ 7kyu/isRubyComing.js
@@ -0,0 +1,27 @@
+/*
+You will be given an array of objects (associative arrays in PHP) 
+representing data about developers who have signed up to attend the next 
+coding meetup that you are organising.
+
+Your task is to return:
+
+true if at least one Ruby developer has signed up; or
+false if there will be no Ruby developers.
+
+Example
+
+var list1 = [
+  { firstName: 'Emma', lastName: 'Z.', country: 'Netherlands', continent: 'Europe', age: 29, language: 'Ruby' },
+  { firstName: 'Piotr', lastName: 'B.', country: 'Poland', continent: 'Europe', age: 128, language: 'Javascript' },
+  { firstName: 'Jayden', lastName: 'P.', country: 'Jamaica', continent: 'Americas', age: 42, language: 'JavaScript' }
+];  ===>  true
+*/
+
+function isRubyComing(list) {
+  // thank you for checking out my kata :)
+  return (
+    list.filter((developer) => {
+      return developer.language === "Ruby";
+    }).length !== 0
+  );
+}
